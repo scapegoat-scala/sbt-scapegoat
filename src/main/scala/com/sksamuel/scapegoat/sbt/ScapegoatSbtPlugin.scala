32160d16765d22ffa59c8e2a7ebc76181d44e34a
sksamuel <sam@sksamuel.com>
2014-07-13 23:24:27 +0100

    Implemented initial pluign

--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -1,19 +1,44 @@
 package com.sksamuel.scapegoat.sbt
 
+import sbt.Keys._
 import sbt._
 
 /** @author Stephen Samuel */
 class ScapegoatSbtPlugin extends Plugin {
 
+  val OrgScoverage = "com.sksamuel"
+  val ScalacArtifact = "scalac-scapegoat-plugin"
+  val ScoverageVersion = "0.1.0"
+
   object Keys {
-    lazy val scapegoat = taskKey[Boolean]("run scapegoat")
   }
 
   import Keys._
 
+  lazy val ScapegoatCompile: Configuration = config("scapegoat")
+
   val scapegoatSettings: Seq[Setting[_]] = {
-    scapegoat := {
-      true
-    }
+    inConfig(ScapegoatCompile)(Defaults.compileSettings) ++
+      Seq(
+        sources in ScapegoatCompile <<= (sources in Compile),
+        sourceDirectory in ScapegoatCompile <<= (sourceDirectory in Compile),
+        resourceDirectory in ScapegoatCompile <<= (resourceDirectory in Compile),
+        resourceGenerators in ScapegoatCompile <<= (resourceGenerators in Compile),
+        externalDependencyClasspath in ScapegoatCompile <<= Classpaths.concat(externalDependencyClasspath in ScapegoatCompile, externalDependencyClasspath in Compile),
+        internalDependencyClasspath in ScapegoatCompile <<= (internalDependencyClasspath in Compile),
+        scalacOptions in ScapegoatCompile <++= update map {
+          (report) =>
+            // find all deps for this configuration
+            val scapegoatDependencies = report matching configurationFilter(ScapegoatCompile.name)
+            // ensure we have the scapegoat dependency on the classpath and if so add it as a scalac plugin
+            scapegoatDependencies.find(_.getAbsolutePath.contains(ScalacArtifact)) match {
+              case None => throw new Exception(s"Fatal: $ScalacArtifact not in libraryDependencies")
+              case Some(classpath) =>
+                Seq(
+                  "-Xplugin:" + classpath.getAbsolutePath
+                )
+            }
+        }
+      )
   }
 }
