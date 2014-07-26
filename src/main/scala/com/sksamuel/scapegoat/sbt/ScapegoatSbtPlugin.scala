0cc2b982d1f241defadf3ee8ef1c31bddff04936
sksamuel <sam@sksamuel.com>
2014-07-26 16:37:24 +0100

    Updated plugin to be an auto plugin

--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -4,41 +4,39 @@ import sbt.Keys._
 import sbt._
 
 /** @author Stephen Samuel */
-object ScapegoatSbtPlugin extends sbt.Plugin {
+object ScapegoatSbtPlugin extends AutoPlugin {
 
   val GroupId = "com.sksamuel.scapegoat"
   val ArtifactId = "scalac-scapegoat-plugin"
-  val Version = "0.1.0"
+  val Version = "0.2.0"
 
-  object Keys {
+  object autoImport {
     lazy val scapegoat = taskKey[Boolean]("scapegoat")
   }
 
-  import com.sksamuel.scapegoat.sbt.ScapegoatSbtPlugin.Keys._
+  import autoImport._
 
-  lazy val scapegoatSettings: Seq[Setting[_]] = {
-      Seq(
-        libraryDependencies += {
-          GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % Version % Compile.name
-        },
-        scalacOptions in Compile <++= update map {
-          (report) =>
-            // find all deps for this configuration
-            val scapegoatDependencies = report matching configurationFilter(Compile.name)
-            // ensure we have the scapegoat dependency on the classpath and if so add it as a scalac plugin
-            scapegoatDependencies.find(_.getAbsolutePath.contains(ArtifactId)) match {
-              case None => throw new Exception(s"Fatal: $ArtifactId not in libraryDependencies")
-              case Some(classpath) =>
-                Seq(
-                  "-Xplugin:" + classpath.getAbsolutePath
-                )
-            }
-        },
-        scapegoat := {
-          val analyis = (compile in Compile).value
-          streams.value.log.info("[scapegoat] generating report")
-          true
+  override lazy val buildSettings = Seq(
+    libraryDependencies += {
+      GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % Version % Compile.name
+    },
+    scalacOptions in Compile <++= update map {
+      (report) =>
+        // find all deps for this configuration
+        val scapegoatDependencies = report matching configurationFilter(Compile.name)
+        // ensure we have the scapegoat dependency on the classpath and if so add it as a scalac plugin
+        scapegoatDependencies.find(_.getAbsolutePath.contains(ArtifactId)) match {
+          case None => throw new Exception(s"Fatal: $ArtifactId not in libraryDependencies")
+          case Some(classpath) =>
+            Seq(
+              "-Xplugin:" + classpath.getAbsolutePath
+            )
         }
-      )
-  }
+    },
+    scapegoat := {
+      val analyis = (compile in Compile).value
+      streams.value.log.info("[scapegoat] generating report")
+      true
+    }
+  )
 }
