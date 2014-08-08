9aedae4d7cdcfaf9cf1147f93a9ad3bba049c2dc
sksamuel <sam@sksamuel.com>
2014-08-08 21:08:50 +0100

    Added verbose mode. Updated keys to be scapegoat* prefixed

--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -8,14 +8,15 @@ object ScapegoatSbtPlugin extends AutoPlugin {
 
   val GroupId = "com.sksamuel.scapegoat"
   val ArtifactId = "scalac-scapegoat-plugin"
-  val Version = "0.90.6"
+  val Version = "0.90.7"
 
   object autoImport {
     lazy val disabledInspections = settingKey[Seq[String]]("Inspections that are disabled globally")
     lazy val scapegoatMaxErrors = settingKey[Int]("Maximum number of errors before the build will fail")
     lazy val scapegoatMaxWarnings = settingKey[Int]("Maximum number of warnings before the build will fail")
-    lazy val consoleOutput = settingKey[Boolean]("Output results of scan to the console during compilation")
-    lazy val outputPath = settingKey[String]("Directory where reports will be written")
+    lazy val scapegoatConsoleOutput = settingKey[Boolean]("Output results of scan to the console during compilation")
+    lazy val scapegoatOutputPath = settingKey[String]("Directory where reports will be written")
+    lazy val scapegoatVerbose = settingKey[Boolean]("Verbose mode for inspections")
   }
 
   import autoImport._
@@ -24,10 +25,11 @@ object ScapegoatSbtPlugin extends AutoPlugin {
   override lazy val projectSettings = Seq(
     disabledInspections := Nil,
     libraryDependencies ++= Seq(
-      GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % Version
+      GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % Version % Compile.name
     ),
-    consoleOutput := true,
-    outputPath := (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report",
+    scapegoatConsoleOutput := true,
+    scapegoatVerbose := false,
+    scapegoatOutputPath := (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report",
     scalacOptions in(Compile, compile) ++= {
       // find all deps for the compile scope
       val scapegoatDependencies = update.value matching configurationFilter(Compile.name)
@@ -35,14 +37,15 @@ object ScapegoatSbtPlugin extends AutoPlugin {
       scapegoatDependencies.find(_.getAbsolutePath.contains(ArtifactId)) match {
         case None => throw new Exception(s"Fatal: $ArtifactId not in libraryDependencies")
         case Some(classpath) =>
-          val path = outputPath.value
+          val path = scapegoatOutputPath.value
           streams.value.log.info(s"[scapegoat] setting output dir to [$path]")
           val disabled = disabledInspections.value
           if (disabled.size > 0)
             streams.value.log.info(s"[scapegoat] disabling inspections: " + disabled.mkString(","))
           Seq(
             "-Xplugin:" + classpath.getAbsolutePath,
-            "-P:scapegoat:consoleOutput:" + consoleOutput.value,
+            "-P:scapegoat:verbose:" + scapegoatVerbose.value,
+            "-P:scapegoat:consoleOutput:" + scapegoatConsoleOutput.value,
             "-P:scapegoat:dataDir:" + path,
             "-P:scapegoat:disabledInspections:" + disabled.mkString(":")
           )
