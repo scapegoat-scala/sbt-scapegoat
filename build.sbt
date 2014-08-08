9976711aa33456377b4a17630a193756498a0594
sksamuel <sam@sksamuel.com>
2014-08-08 09:26:18 +0100

    Added console output #1

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "sbt-scapegoat"
 
 organization := "com.sksamuel.scapegoat"
 
-version := "0.90.5"
+version := "0.90.6"
 
 scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8")
 
--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -8,13 +8,14 @@ object ScapegoatSbtPlugin extends AutoPlugin {
 
   val GroupId = "com.sksamuel.scapegoat"
   val ArtifactId = "scalac-scapegoat-plugin"
-  val Version = "0.90.5"
+  val Version = "0.90.6"
 
   object autoImport {
-    lazy val myTask = taskKey[String]("qweqwe")
     lazy val disabledInspections = settingKey[Seq[String]]("Inspections that are disabled globally")
     lazy val scapegoatMaxErrors = settingKey[Int]("Maximum number of errors before the build will fail")
     lazy val scapegoatMaxWarnings = settingKey[Int]("Maximum number of warnings before the build will fail")
+    lazy val consoleOutput = settingKey[Boolean]("Output results of scan to the console during compilation")
+    lazy val outputPath = settingKey[String]("Directory where reports will be written")
   }
 
   import autoImport._
@@ -25,6 +26,8 @@ object ScapegoatSbtPlugin extends AutoPlugin {
     libraryDependencies ++= Seq(
       GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % Version
     ),
+    consoleOutput := true,
+    outputPath := (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report",
     scalacOptions in(Compile, compile) ++= {
       // find all deps for the compile scope
       val scapegoatDependencies = update.value matching configurationFilter(Compile.name)
@@ -32,14 +35,15 @@ object ScapegoatSbtPlugin extends AutoPlugin {
       scapegoatDependencies.find(_.getAbsolutePath.contains(ArtifactId)) match {
         case None => throw new Exception(s"Fatal: $ArtifactId not in libraryDependencies")
         case Some(classpath) =>
-          val target = (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report"
-          streams.value.log.info(s"[scapegoat] setting output dir to [$target]")
+          val path = outputPath.value
+          streams.value.log.info(s"[scapegoat] setting output dir to [$path]")
           val disabled = disabledInspections.value
           if (disabled.size > 0)
             streams.value.log.info(s"[scapegoat] disabling inspections: " + disabled.mkString(","))
           Seq(
             "-Xplugin:" + classpath.getAbsolutePath,
-            "-P:scapegoat:dataDir:" + target,
+            "-P:scapegoat:consoleOutput:" + consoleOutput.value,
+            "-P:scapegoat:dataDir:" + path,
             "-P:scapegoat:disabledInspections:" + disabled.mkString(":")
           )
       }
