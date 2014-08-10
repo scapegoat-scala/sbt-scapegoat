8d1cb0e1581942fa380db737fa2e761cf35fdf52
sksamuel <sam@sksamuel.com>
2014-08-10 22:37:07 +0100

    Added enabled inspections option

--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -12,6 +12,7 @@ object ScapegoatSbtPlugin extends AutoPlugin {
   object autoImport {
     lazy val scapegoatVersion = settingKey[String]("The version of the scala plugin to use")
     lazy val disabledInspections = settingKey[Seq[String]]("Inspections that are disabled globally")
+    lazy val enabledInspections = settingKey[Seq[String]]("Inspections that are explicitly enabled")
     lazy val scapegoatMaxErrors = settingKey[Int]("Maximum number of errors before the build will fail")
     lazy val scapegoatMaxWarnings = settingKey[Int]("Maximum number of warnings before the build will fail")
     lazy val scapegoatMaxInfos = settingKey[Int]("Maximum number of infos before the build will fail")
@@ -24,7 +25,6 @@ object ScapegoatSbtPlugin extends AutoPlugin {
 
   override def trigger = allRequirements
   override lazy val projectSettings = Seq(
-    disabledInspections := Nil,
     scapegoatVersion := "0.90.11",
     libraryDependencies ++= Seq(
       GroupId % (ArtifactId + "_" + scalaBinaryVersion.value) % scapegoatVersion.value % Compile.name
@@ -34,6 +34,8 @@ object ScapegoatSbtPlugin extends AutoPlugin {
     scapegoatMaxInfos := -1,
     scapegoatMaxWarnings := -1,
     scapegoatMaxErrors := -1,
+    disabledInspections := Nil,
+    enabledInspections := Nil,
     scapegoatOutputPath := (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report",
     scalacOptions in(Compile, compile) ++= {
       // find all deps for the compile scope
@@ -44,15 +46,22 @@ object ScapegoatSbtPlugin extends AutoPlugin {
         case Some(classpath) =>
           val path = scapegoatOutputPath.value
           streams.value.log.info(s"[scapegoat] setting output dir to [$path]")
+
           val disabled = disabledInspections.value
           if (disabled.size > 0)
-            streams.value.log.info(s"[scapegoat] disabling inspections: " + disabled.mkString(","))
+            streams.value.log.info(s"[scapegoat] disabled inspections: " + disabled.mkString(","))
+
+          val enabled = enabledInspections.value
+          if (enabled.size > 0)
+            streams.value.log.info(s"[scapegoat] enabled inspections: " + enabled.mkString(","))
+
           Seq(
             "-Xplugin:" + classpath.getAbsolutePath,
             "-P:scapegoat:verbose:" + scapegoatVerbose.value,
             "-P:scapegoat:consoleOutput:" + scapegoatConsoleOutput.value,
             "-P:scapegoat:dataDir:" + path,
-            "-P:scapegoat:disabledInspections:" + disabled.mkString(":")
+            "-P:scapegoat:disabledInspections:" + disabled.mkString(":"),
+            "-P:scapegoat:enabledInspections:" + enabled.mkString(":")
           )
       }
     }
