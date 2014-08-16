657afd665792c6d87c170d3aeb6681ce160c554f
sksamuel <sam@sksamuel.com>
2014-08-16 06:45:22 +0100

    Added ignored file patterns option

--- src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
+++ src/main/scala/com/sksamuel/scapegoat/sbt/ScapegoatSbtPlugin.scala
@@ -13,6 +13,7 @@ object ScapegoatSbtPlugin extends AutoPlugin {
     lazy val scapegoatVersion = settingKey[String]("The version of the scala plugin to use")
     lazy val disabledInspections = settingKey[Seq[String]]("Inspections that are disabled globally")
     lazy val enabledInspections = settingKey[Seq[String]]("Inspections that are explicitly enabled")
+    lazy val scapegoatIgnoredFiles = settingKey[Seq[String]]("File patterns to ignore")
     lazy val scapegoatMaxErrors = settingKey[Int]("Maximum number of errors before the build will fail")
     lazy val scapegoatMaxWarnings = settingKey[Int]("Maximum number of warnings before the build will fail")
     lazy val scapegoatMaxInfos = settingKey[Int]("Maximum number of infos before the build will fail")
@@ -36,6 +37,7 @@ object ScapegoatSbtPlugin extends AutoPlugin {
     scapegoatMaxErrors := -1,
     disabledInspections := Nil,
     enabledInspections := Nil,
+    scapegoatIgnoredFiles := Nil,
     scapegoatOutputPath := (crossTarget in Compile).value.getAbsolutePath + "/scapegoat-report",
     scalacOptions in(Compile, compile) ++= {
       // find all deps for the compile scope
@@ -61,7 +63,8 @@ object ScapegoatSbtPlugin extends AutoPlugin {
             "-P:scapegoat:consoleOutput:" + scapegoatConsoleOutput.value,
             "-P:scapegoat:dataDir:" + path,
             "-P:scapegoat:disabledInspections:" + disabled.mkString(":"),
-            "-P:scapegoat:enabledInspections:" + enabled.mkString(":")
+            "-P:scapegoat:enabledInspections:" + enabled.mkString(":"),
+            "-P:scapegoat:ignoredFiles:" + enabled.mkString(":")
           )
       }
     }
