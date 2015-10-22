01d39f4b2f64b3120051eb8cd1fa2f4b66e0a045
Richard Bradley <Richard.Bradley@softwire.com>
2015-10-22 10:00:58 +0100

    #97 Add a basic-operation example test to verify that sbt-scapegoat is working

---
+++ src/sbt-test/scapegoat/basic-operation/src/main/scala/my/app/Example.scala
@@ -0,0 +1,10 @@
+package my.app
+
+object Example {
+
+  def exampleCodeUsingArrayToString(a: Array[Int]): Unit = {
+    // This is just some example code which demonstrates that the Inspection
+    // "ArraysToString" is working.
+    println("The array contained: " + a.toString)
+  }
+}
